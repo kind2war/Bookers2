60be7d602fbd914513123f90565966a0ae92361f
kind2war <130664778+kind2war@users.noreply.github.com>
2023-05-26 07:40:41 +0000

    [add]UserInfoのレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'
   get 'homes/about' => 'homes#about', as: "about"
-  resources :users 
+  resources :users
   resources :books
 
 end
