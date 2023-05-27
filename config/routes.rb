f081811439151fd1cf876090398704b3aa46dc58
kind2war <130664778+kind2war@users.noreply.github.com>
2023-05-27 20:41:20 +0000

    [add]bookのedit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: "about"
   get "users/:id" => "users#show", as: :mypage
   resources :users, only: [:show, :index, :edit, :new, :destroy]
-  resources :books
-
-
+  resources :books, only: [:show, :index, :edit, :new, :destroy]
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
