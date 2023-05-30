8259715dc6a4e48dc3c799056c2bfd30447e581c
kind2war <130664778+kind2war@users.noreply.github.com>
2023-05-30 15:20:05 +0000

    log_in_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
   def get_profile_image(width, height)
