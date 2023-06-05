159f9a2eb9be7cc83507770fe7a92679bd681bec
kind2war <130664778+kind2war@users.noreply.github.com>
2023-06-05 08:01:45 +0000

    [fix]header,but sidebar_new_book_has_bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'
-  get 'homes/about' => 'homes#about', as: "about"
+  get 'home/about' => 'homes#about', as: "about"
   get "users/:id" => "users#show", as: :mypage
   resources :users , only: [:show, :index, :edit, :new, :destroy, :update]
   resources :books
